f6992ed24dc83c0a52c610c4fbfcfcc3f6a894d9
vishnu venu <vishnuvenuvenk@gmail.com>
2019-08-30 13:24:55 +0530

    added elastic search package

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
+import os
 from flask import Flask, request, jsonify
 import json
 from waitress import serve
+from elasticsearch import Elasticsearch
+from ssl import create_default_context
 
-import os
+context = create_default_context(cafile="path/to/cafile.pem")
+
+
+es = Elasticsearch("https://157.245.111.179:9200",
+                   ssl_context=context, http_auth=('admin', 'admin'))
 
 
 app = Flask(__name__)
@@ -10,11 +17,46 @@ app = Flask(__name__)
 PORT = 8080
 
 
-@app.route("/")
-def entry():
+@app.route('/', methods=['GET'])
+def index():
+    results = es.get(index='contents', doc_type='title', id='my-new-slug')
+    return jsonify(results['_source'])
 
-    print("testing")
-    return "working"
+
+@app.route('/insert_data', methods=['POST'])
+def insert_data():
+    slug = request.form['slug']
+    title = request.form['title']
+    content = request.form['content']
+
+    body = {
+        'slug': slug,
+        'title': title,
+        'content': content,
+
+    }
+
+    result = es.index(index='contents', doc_type='title', id=slug, body=body)
+
+    return jsonify(result)
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    keyword = request.form['keyword']
+
+    body = {
+        "query": {
+            "multi_match": {
+                "query": keyword,
+                "fields": ["content", "title"]
+            }
+        }
+    }
+
+    res = es.search(index="contents", doc_type="title", body=body)
+
+    return jsonify(res['hits']['hits'])
 
 
 if __name__ == "__main__":
