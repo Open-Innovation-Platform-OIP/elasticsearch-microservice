27cf986b4fcff656980a061bb5deaa70377199b9
vishnu venu <vishnuvenuvenk@gmail.com>
2019-08-30 13:37:27 +0530

    added certs

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from elasticsearch import Elasticsearch
 
 
 es = Elasticsearch("https://157.245.111.179:9200",
-                   http_auth=('admin', 'admin'), verify_certs=False, ca_certs=False,)
+                   http_auth=('admin', 'admin'), ca_certs=False, verify_certs=False)
 
 
 app = Flask(__name__)
