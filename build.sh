7d03e920c6ceb0005abaf1d2336be309d15c1489
Tej Pochiraju <tej@jaaga.in>
2020-02-18 14:33:28 +0530

    use variables in build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # access_token = 5b5efa54-6113-48e2-9053-79ce9b3b04f4
+NAME_SPACE="socialalphaoip"
+IMAGE_NAME="elasticsearch-microservice"
+TAG="prod"
 docker login --username tejpochiraju --password 5b5efa54-6113-48e2-9053-79ce9b3b04f4
-docker build -t socialalphaoip/elasticsearch-microservice:prod .
-docker push socialalphaoip/elasticsearch-microservice:prod
+docker build -t $NAME_SPACE/$IMAGE_NAME:$TAG .
+docker push $NAME_SPACE/$IMAGE_NAME:$TAG
