673a8cb66a4d2c3dcfbf2391a44fd90664a749bc
Tej Pochiraju <tej@jaaga.in>
2020-02-18 14:11:23 +0530

    added build script

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# access_token = 5b5efa54-6113-48e2-9053-79ce9b3b04f4
+docker login --username tejpochiraju --password 5b5efa54-6113-48e2-9053-79ce9b3b04f4
+docker build -t socialalphaoip/elasticsearch-microservice:prod .
+docker push socialalphaoip/elasticsearch-microservice:prod
